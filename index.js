07e07eea8e037fc55bbf1b070345adbcbc256f72
asaf-ashwal <ashwalasafx@gmail.com>
2024-02-13 15:31:20 +0200

    route lead

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3003;
-const cors =require('cors')
+const cors =require('cors');
+const leadRouter = require("./router/lead.router");
 app.use(cors())
 app.use(express.json())
+app.use('/api/lead',leadRouter);
 require('./DL/db').connect()
 
 app.listen(port, () => console.log(`server is running on port ${port} `));
