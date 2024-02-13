0fb4e02847e8c693e12e17d58319bdd0ec460aca
Avi <avdoitsh@gmail.com>
2024-02-13 16:27:41 +0200

    controler end model users

--- DL/controllers/user.controller.js
+++ DL/controllers/user.controller.js
@@ -1,1 +1,34 @@
 const userModel = require("../models/user.model");
+
+const create = async (data) => {
+    const result = await userModel.create(data)
+    console.log(result);
+    return result
+}
+
+const read = async (filter) => {
+    const result = await userModel.find(filter)
+    console.log(result);
+    return result
+}
+
+const readOne = async (filter) => {
+    const result = await userModel.findOne(filter)
+    console.log(result);
+    return result
+}
+
+const updateOne = async (filter, newData) => {
+    const result = await userModel.findOneAndUpdate(filter, newData, { new: true })
+    console.log(result);
+    return result
+}
+
+const deleteOne = async (filter, isActive) => {
+    const result = await userModel.findOneAndUpdate(filter, { isActive: false })
+    console.log(result);
+    return result
+}
+
+
+module.exports = { create, deleteOne, updateOne, readOne, read }
