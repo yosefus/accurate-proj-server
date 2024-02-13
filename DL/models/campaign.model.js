97bd802170e05a6ceedc8bc64df4fbdd675956dd
asaf-ashwal <ashwalasafx@gmail.com>
2024-02-13 17:50:53 +0200

    campaign model

---
+++ DL/models/campaign.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const msgSchema = new mongoose.Schema({
+  subject: {type: String, required: true},
+  isActive: {type: Boolean, default: true},
+  content: {type: String, required: true},
+  creationDate: {type: Date, default: Date.now},
+  status: {required: true, enun: ["sent", "draft"], default: "draft"},
+  leads: [
+    {
+      lead: {type: mongoose.SchemaTypes.ObjectId, ref: "lead", required: true},
+      receptionDate: {type: Date, default: Date.now},
+      status: {required: true, enun: ["sent", "recived"], default: "sent"},
+    },
+  ],
+});
+
+const campSchema = new mongoose.Schema({
+  // משתמש
+  user: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  // כותרת
+  title: {
+    type: String,
+    required: true,
+  },
+  // תוכן
+  details: {
+    type: String,
+  },
+  isActive: {type: Boolean, default: true},
+
+  // לידים - מערך של אובייקטים
+  leads: [
+    {
+      lead: {type: mongoose.SchemaTypes.ObjectId, ref: "lead"},
+      isActive: {type: Boolean, default: true},
+      joinDate: {type: Date, default: Date.now},
+    },
+  ],
+  msg: [msgSchema],
+});
+
+const campaignModel = mongoose.model("campaign", campSchema);
+module.exports = campaignModel;
