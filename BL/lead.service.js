ee4c34c4c7fa594bf4fda9aabda45affadc5f8b0
davidshayo <davidshayoo@gmail.com>
2024-02-14 09:13:29 +0200

    create service, router and controller for lead

--- BL/lead.service.js
+++ BL/lead.service.js
@@ -1,1 +1,27 @@
-const leadController = require("../DL/controllers/lead.controller");
+
+const {create, update, readOne} = require("../DL/controllers/lead.controller");
+
+const createOneLead = async (lead) => { 
+    
+    if (!lead.Fname || !lead.Lname || !lead.email || !lead.phone  ){
+        throw {msg: 'אנא מלא את כל שדות החובה'}
+        }
+
+        const exist = await readOne({phone: lead.phone})
+        if(exist) return exist
+
+     else{
+         const creatLead =  await create (lead)
+         return creatLead
+     }
+     }       
+    
+
+    const updateLead = async (idLead, newLead) =>{
+     const updatedLead =   await update ({_id: idLead}, newLead)
+        return updatedLead
+    
+    }
+
+
+ module.exports = {createOneLead, updateLead}
