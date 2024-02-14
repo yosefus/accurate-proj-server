7066573d77f3f5baf7132ada71bb1318123a1a86
yosefus <yosalsoni@gmail.com>
2024-02-14 10:47:01 +0200

    fix schema

--- DL/controllers/campaign.controller.js
+++ DL/controllers/campaign.controller.js
@@ -7,11 +7,5 @@ console.log(result);
 return result
 }
 
-connect().then(async()=>{
-    const result = await create( {user: '65c33dc58a4b7bf8aea86b09', title: 'fullstack'} )
-    console.log(result);
-})
-
-
 
 module.exports ={create}
