91cb7649e519e46f16e270ff209d05fdfe7788f3
yosefus <yosalsoni@gmail.com>
2024-02-14 17:29:56 +0200

    k

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3003;
-const cors =require('cors');
+const cors = require('cors');
 
 require('./DL/db').connect()
 
@@ -10,14 +10,13 @@ app.use(cors())
 app.use(express.json())
 
 const leadRouter = require("./router/lead.router");
-app.use('/api/lead',leadRouter);
+app.use('/api/lead', leadRouter);
 
 const campaignRouter = require("./router/campaign.router");
-app.use('/api/campaign',campaignRouter);
+app.use('/api/campaign', campaignRouter);
 
 app.use('*', (req, res) => {
-   res.status(404).json({msg: 'GOT YOU! this route is not create yet! 😘'})
+   res.status(404).json({ msg: 'GOT YOU! this route is not create yet! 😘' })
 })
-
 
 app.listen(port, () => console.log(`server is running on port ${port} `));
